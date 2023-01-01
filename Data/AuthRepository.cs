21747970767b313dfc21e82f866bcf0b3d98ec29
Christerresin <christer.resin@gmail.com>
2023-01-01 13:40:01 +0100

    create CreatePasswordHash helper and implement inside register

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -7,19 +7,43 @@ namespace dotnet_rpg.Data
 {
   public class AuthRepository : IAuthRepository
   {
+    private readonly DataContext context;
+
+    public AuthRepository(DataContext context)
+    {
+      this.context = context;
+    }
     public Task<ServiceResponse<string>> Login(string username, string password)
     {
       throw new NotImplementedException();
     }
 
-    public Task<ServiceResponse<int>> Register(User user, string password)
+    public async Task<ServiceResponse<int>> Register(User user, string password)
     {
-      throw new NotImplementedException();
+      CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+      user.passwordHash = passwordHash;
+      user.passwordSalt = passwordSalt;
+
+      context.Users.Add(user);
+      await context.SaveChangesAsync();
+      ServiceResponse<int> response = new ServiceResponse<int>();
+      response.Data = user.Id;
+      return response;
     }
 
     public Task<bool> UserExists(string username)
     {
       throw new NotImplementedException();
+    }
+
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+      using (var hmac = new System.Security.Cryptography.HMACSHA512())
+      {
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+      }
     }
   }
 }
